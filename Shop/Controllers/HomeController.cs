e5a73106750f76f46ddbc317250bd4c3c8360e4e
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-05-04 20:40:40 -0300

    Remoção de usings não utilizados

--- Shop/Controllers/HomeController.cs
+++ Shop/Controllers/HomeController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Models;
 using Shop.Repositories;
 using Shop.Services;
+using System.Threading.Tasks;
 
 namespace Shop.Controllers
 {
